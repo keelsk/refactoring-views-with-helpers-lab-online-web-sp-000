65fe30d699044d3366c37acbcb53f517cdf85eb9
Kirsten Keels <keelsk@gmail.com>
2020-05-08 16:36:16 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -3,7 +3,7 @@ module ArtistsHelper
     if song.artist.present?
       link_to song.artist_name, artist_path(song.artist)
     else
-      link_to "Add Artist", edit_artist_path(song)
+      link_to "Add Artist", edit_song_path(song)
     end
   end
 end
